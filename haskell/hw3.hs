36fcb55c923f06345305f3399498c2260653ad26
Jake Prather <JakeHP@Zoho.com>
2015-06-09 19:50:10 -0500

    get from list where x < any element in list.

--- haskell/hw3.hs
+++ haskell/hw3.hs
@@ -1,3 +1,5 @@
+-- See https://github.com/Jakehp/PLC/blob/master/pdfs/hwk3.pdf
+
 insert :: Int -> [Int] -> [Int]
 insert x [] = [x]
 insert x (y:ys) =
@@ -19,3 +21,14 @@ sqsumHelper cur sum = sqsumHelper (cur-1) (sum+cur*cur)
 
 sqsum :: Int -> Int
 sqsum x = sqsumHelper x 0
+
+lessHelper :: Int -> [Int] -> [Int] -> [Int]
+lessHelper someNum [] lessThanList = lessThanList
+lessHelper someNum (x:xs) lessThanList =
+    if someNum < x then
+        lessHelper someNum xs (x:lessThanList)
+    else
+        lessHelper someNum xs lessThanList
+
+less :: (Int, [Int]) -> [Int]
+less (x, y:ys) = lessHelper x (y:ys) []
