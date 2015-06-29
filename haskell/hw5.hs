aa6f731ba1cb1b172c2700f99220ca3584ec460d
Jake Prather <JakeHP@Zoho.com>
2015-06-28 19:05:53 -0500

    Change function type declaration to use equals typeclass.

--- haskell/hw5.hs
+++ haskell/hw5.hs
@@ -1,4 +1,4 @@
 -- See https://github.com/Jakehp/PLC/blob/master/pdfs/hwk5.pdf
 
-is_member :: [Int] -> Int -> Bool
+is_member :: (Eq a) => [a] -> a -> Bool
 is_member l x = foldl (\hasBeenFound e -> if not hasBeenFound then x == e else hasBeenFound) False l
