f2cc42f6c706086a46fbcc629c47d27f2cd1c01e
Jake Prather <JakeHP@Zoho.com>
2015-07-16 17:51:29 -0500

    Implemented is_stop_word.

--- haskell/hw5.hs
+++ haskell/hw5.hs
@@ -20,3 +20,18 @@ splitter someSentence
         | otherwise = get_first_word fixedSentence : splitter (removeFirstWord fixedSentence)
     where
         fixedSentence = removeFirstSymbols someSentence
+
+stop_words = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,\
+    \because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,\
+    \for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,\
+    \it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,\
+    \of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,\
+    \some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,\
+    \wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,\
+    \you,your";
+
+-- Not a very good func name for what it does...
+-- are_stop_words is better.
+is_stop_word :: [Char] -> [Bool]
+is_stop_word sentence = map (\e-> is_member (splitter stop_words) e) (splitter sentence)
+-- split words
